cda099ab494b56da5214cfa17c91613cbed94b89
waysterMelo <waystermelo@gmail.com>
2021-09-10 20:07:37 -0300

    september 10 2021

--- src/main/java/main/VendasApp.java
+++ src/main/java/main/VendasApp.java
@@ -1,27 +1,12 @@
 package main;
 
-import main.domain.entity.Cliente;
-import main.domain.repository.Cliente_repo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class VendasApp {
-
-
-    @Bean
-    public CommandLineRunner commandLineRunner(@Autowired Cliente_repo cliente_repo){
-        return args -> {
-            Cliente c = new Cliente(null, "Wayster");
-            cliente_repo.save(c);
-        };
-    }
-
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApp.class, args);
--- src/main/java/main/rest/controller/ClienteController.java
+++ src/main/java/main/rest/controller/ClienteController.java
@@ -32,4 +32,35 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
+
+    @PostMapping("/api/cliente")
+    @ResponseBody
+    public ResponseEntity<Cliente> save(@RequestBody Cliente cliente){
+        Cliente c = cliente_repo.save(cliente);
+        return ResponseEntity.ok(c);
+    }
+
+    @DeleteMapping("/api/cliente/{id}")
+    @ResponseBody
+    public ResponseEntity<Cliente> delete(@PathVariable Integer id){
+        Optional<Cliente> cliente = cliente_repo.findById(id);
+
+        if (cliente.isPresent()){
+            cliente_repo.delete(cliente.get());
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @PutMapping("/api/cliente/{id}")
+    @ResponseBody
+    public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente){
+        return cliente_repo.findById(id).map(c -> {
+            c.setId(c.getId());
+            cliente_repo.save(cliente);
+            return ResponseEntity.noContent().build();
+        }).orElseGet( () -> ResponseEntity.notFound().build());
+    }
+
 }
