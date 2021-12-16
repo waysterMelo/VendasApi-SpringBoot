6c95450992defe5a235105997abe5aa7772944d9
waysterMelo <waystermelo@gmail.com>
2021-12-15 22:52:23 -0300

    15-12-2021

--- src/main/java/main/config/SecurityConfig.java
+++ src/main/java/main/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package main.config;
 
 
+import main.service.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +16,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+    @Autowired
+    private UsuarioServiceImpl usuarioService;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -20,18 +26,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/api/clientes/**").authenticated().and().formLogin();
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/api/clientes/**")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/api/produtos/**")
+                .hasRole("ADMIN")
+                .antMatchers("/api/pedidos/**")
+                .hasAnyRole("USER","ADMIN")
+                .antMatchers(HttpMethod.POST,"/api/usuarios/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("user")
-                .password(passwordEncoder()
-                        .encode("123"))
-                .roles("USER");
-
+        auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder());
     }
 
 
---
+++ src/main/java/main/rest/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package main.rest.controller;
+
+import lombok.RequiredArgsConstructor;
+import main.domain.entity.Usuario;
+import main.service.UsuarioServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+
+    private final UsuarioServiceImpl usuarioService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario salvar(@RequestBody @Valid Usuario usuario){
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
+        return usuarioService.salvar(usuario);
+    }
+
+}
