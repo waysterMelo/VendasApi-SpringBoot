25190f1fc0fc85163734762c038c3905914e12b8
waysterMelo <waystermelo@gmail.com>
2022-07-07 14:45:56 -0300

    clean code

--- src/main/java/main/validation/constraintValidation/notEmptyListValidator.java
+++ src/main/java/main/validation/constraintValidation/notEmptyListValidator.java
@@ -2,10 +2,8 @@ package main.validation.constraintValidation;
 
 import main.validation.NotEmptyList;
 
-import javax.validation.Constraint;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import javax.validation.ConstraintViolation;
 import java.util.List;
 
 public class notEmptyListValidator implements ConstraintValidator<NotEmptyList, List> {
